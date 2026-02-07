6457b40b200b3f929f1afef443b8e4d393c60e0b
renardbergson <renard.contato@gmail.com>
2026-02-07 15:31:07 -0300

    refactor(sidebar): refactor sidebar with data-driven menus

--- src/app/pages/dashboard/components/sidebar/sidebar.ts
+++ src/app/pages/dashboard/components/sidebar/sidebar.ts
@@ -9,19 +9,32 @@ import { RouterModule } from '@angular/router';
   styleUrl: './sidebar.css',
 })
 export class Sidebar implements OnInit {
+  isCollapsed: boolean = false;
+  isAdmin: boolean = true;
+
+  menuItems = [
+    { text: "Dashboard", icon: "bi bi-grid-1x2", route: "/user/dashboard" },
+    { text: "Clientes", icon: "bi bi-people", route: "/user/customers" },
+    { text: "Compras", icon: "bi bi-cart", route: "/user/purchases" },
+    { text: "Cashbacks", icon: "bi bi-gift", route: "/user/cashbacks" }
+  ];
+
+  adminMenuItems = [
+    { text: "Relatórios", icon: "bi bi-bar-chart", route: "/admin/reports" },
+    { text: "Funcionários", icon: "bi bi-people", route: "/admin/employees" },
+  ];
+
+  userInfo = {
+    name: 'João da Silva',
+    role: 'Administrador',
+    initials: 'JD'
+  }
+
   @HostBinding('class.collapsed') get collapsedClass() {
     return this.isCollapsed;
   }
 
   @Output() collapsedChange = new EventEmitter<boolean>();
-
-  isCollapsed: boolean = false;
-
-  // Dados mockados do usuário
-  userName: string = 'João da Silva';
-  userRole: string = 'Administrador';
-  userInitials: string = 'JD';
-  isAdmin: boolean = true;
 
   ngOnInit(): void {
     // Carregar estado salvo do localStorage
